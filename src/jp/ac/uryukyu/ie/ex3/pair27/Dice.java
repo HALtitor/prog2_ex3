c2be30d4711a0c7289e41b1319fd7c8a735250f4
Yamakawa Haruki <e175753@ie.u-ryukyu.ac.jp>
2017-11-14 11:43:16 +0900

    first

---
+++ src/jp/ac/uryukyu/ie/ex3/pair27/Dice.java
@@ -0,0 +1,18 @@
+package jp.ac.uryukyu.ie.ex3.pair27;
+
+public class Dice{
+    private int value; //出目を保存する変数
+    public Dice(){
+	play();
+    }
+    public int getValue(){
+	return value;
+    }
+    public void setValue(int value){
+	this.value = value;
+    }
+    public void play(){
+	int ran=(int)(Math.random()*6) + 1;
+	this.value = ran;
+    }
+}
